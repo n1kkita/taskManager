d47422a146cf8ce6a784d7d21de0f42feca0b9d3
Никита <128186146+n1kkita@users.noreply.github.com>
2023-08-11 19:26:19 +0300

    create entity Task(id,title,description,dateOfCreate,dateOfEnd,status)

---
+++ src/main/java/com/example/taskmanager/models/Status.java
@@ -0,0 +1,8 @@
+package com.example.taskmanager.models;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public enum Status {
+    IN_PROCESS,DONE,NOT_DONE
+}
---
+++ src/main/java/com/example/taskmanager/models/Task.java
@@ -0,0 +1,35 @@
+package com.example.taskmanager.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "tasks")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    private LocalDateTime dateOfCreate;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private Date dateOfEnd;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    @PrePersist
+    public void init(){
+        dateOfCreate = LocalDateTime.now();
+    }
+}
