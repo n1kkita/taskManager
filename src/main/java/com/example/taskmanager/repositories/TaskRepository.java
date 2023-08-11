f40f2b515032d6cf3ae84822cd16f97b6cb0d330
Никита <128186146+n1kkita@users.noreply.github.com>
2023-08-11 20:16:59 +0300

    corrected the mistake in repository

--- src/main/java/com/example/taskmanager/repositories/TaskRepository.java
+++ src/main/java/com/example/taskmanager/repositories/TaskRepository.java
@@ -14,7 +14,7 @@ public interface TaskRepository extends JpaRepository< Task, Long > {
 
     @Query("select new com.example.taskmanager.dto.TaskDto(t.title,t.description,t.status,t.dateOfEnd) from Task t")
     List< TaskDto > findAllTasks();
-    @Query("select new com.example.taskmanager.dto.TaskDto(t.title,t.description,t.status,t.dateOfEnd) from Task t")
+    @Query("select new com.example.taskmanager.dto.TaskDto(t.title,t.description,t.status,t.dateOfEnd) from Task t where t.id = ?1")
 
     TaskDto findTaskById(Long id);
 }
